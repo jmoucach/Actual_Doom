e36ec6d8d6cbb97218a20cc1006a78996eef71c3
Judith <jpoulvel@student.42.fr>
2020-04-14 19:20:13 +0200

    commenting unuseful lines

--- map_editor/src/draw/draw_menu.c
+++ map_editor/src/draw/draw_menu.c
@@ -12,7 +12,7 @@
 
 #include "../../hdr/map_editor.h"
 
-void			ft_print_rectangle(t_fdf *img /*SDL_Renderer *ren*/, int x, int y, int w, int h)
+void			ft_print_rectangle(t_fdf *img, int x, int y, int w, int h)
 {
 	/*t_pt			pt_a;
 	t_pt			pt_b;
@@ -61,15 +61,15 @@ void			draw_textures_squares(t_fdf *img)
 		if (i == 12)
 			i++;
 		ft_print_blocks(1320 + (((i - 1) % 3) * 80), 120 + (((i - 1) / 3) * 80), i, img, 70);
-		ft_print_rectangle(img/*->renderer*/, 1320 + (((i - 1) % 3) * 80) - 1, 120 + (((i  - 1 ) / 3) * 80) - 1, 70 + 2, 70 + 2);
+		ft_print_rectangle(img, 1320 + (((i - 1) % 3) * 80) - 1, 120 + (((i  - 1 ) / 3) * 80) - 1, 70 + 2, 70 + 2);
 	}
 }
 
 void			ft_render_buttons(t_fdf *img)
 {
 	//SDL_SetRenderDrawColor(img->renderer, 215, 215, 215, 0);
-	ft_print_rectangle(img/*->renderer*/, 1320 - 1, 280 + 500 - 1, 230 + 2, 60 + 2);//rem wall
-	ft_print_rectangle(img/*->renderer*/, 1320 - 1, 360 + 500 - 1, 230 + 2, 60 + 2);//save wall
+	ft_print_rectangle(img, 1320 - 1, 280 + 500 - 1, 230 + 2, 60 + 2);//rem wall
+	ft_print_rectangle(img, 1320 - 1, 360 + 500 - 1, 230 + 2, 60 + 2);//save wall
 	print_button_add_and_substract(img);
 	draw_textures_squares(img);
 }
@@ -82,16 +82,16 @@ void			ft_print_pressed_button(t_fdf *img, int i)
 	//SDL_SetRenderDrawColor(img->renderer, 255, 0, 0, 0);
 	if (i == 12)
 		return ;
-	if (i == 0)
+	/*if (i == 0)//Seems that nothing changes when I (un)comment this
 	{
-		ft_print_rectangle(img/*->renderer*/, 1319, 279 + 500, 232, 62);
-		ft_print_rectangle(img/*->renderer*/, 1318, 278 + 500, 234, 64);
+		ft_print_rectangle(img, 1319, 279 + 500, 232, 62);
+		ft_print_rectangle(img, 1318, 278 + 500, 234, 64);
 	}
 	else
 	{
 		startx = 1320 + (((i - 1) % 3) * 80);
 		starty = 120 + (((i - 1) / 3) * 80);
-		ft_print_rectangle(img/*->renderer*/, startx - 1, starty - 1, 72, 72);
-		ft_print_rectangle(img/*->renderer*/, startx - 2, starty - 2, 74, 74);
-	}
+		ft_print_rectangle(img, startx - 1, starty - 1, 72, 72);
+		ft_print_rectangle(img, startx - 2, starty - 2, 74, 74);
+	}*/
 }
