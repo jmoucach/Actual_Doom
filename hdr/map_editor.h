289fe11ff5c68073405c6a7505142305e7876889
Judith <jpoulvel@student.42.fr>
2020-04-22 18:22:29 +0200

    main norm on map_editor.h but still need to remove the comments and uncomment the correct path to SDL and SDL_ttf

--- hdr/map_editor.h
+++ hdr/map_editor.h
@@ -18,12 +18,10 @@
 //# include "/usr/local/include/SDL2/SDL_TTF.h"
 //# include "/usr/include/SDL2/SDL.h"
 //# include "/usr/include/SDL2/SDL_ttf.h"
-
 # include <SDL.h>
 # include <SDL_ttf.h>
 # include "../libft/inc/get_next_line.h"
 # include <fcntl.h>
-# include <stdio.h>//DEBUG
 # include <math.h>
 
 # define WIDTH 1400
@@ -53,7 +51,7 @@ typedef struct		s_pt
 {
 	int				x;
 	int				y;
-} 					t_pt;
+}					t_pt;
 
 typedef struct		s_mouse
 {
@@ -73,23 +71,7 @@ typedef	struct		s_key
 	int				zoom_in;
 	int				zoom_out;
 }					t_key;
-/*
-typedef struct		s_item
-{
-	SDL_Rect		rect;
-	SDL_Surface		*surface;
-	SDL_Texture		*texture;
-}					t_item;
 
-typedef struct		s_txt
-{
-	t_item			*menu;
-	t_item			*item;
-	TTF_Font		*font;
-	SDL_Rect		txt_through;
-	SDL_Color		color;
-}					t_txt;
-*/
 typedef struct		s_map
 {
 	int				**map;
@@ -98,9 +80,7 @@ typedef struct		s_map
 	int				ox;
 	int				oy;
 	float			base_gap;
-	float			base_h;
 	char			proj;
-	int				zmax;
 	int				endx;
 	int				endy;
 }					t_map;
@@ -110,11 +90,8 @@ typedef struct		s_fdf
 	char			*map_name;
 	SDL_Window		*window;
 	SDL_Renderer	*renderer;
-	//SDL_Texture		*map_texture;
 	SDL_Texture		**texture;
 	SDL_Texture		*grid;
-	//SDL_Texture		*text_texture;
-	//t_txt			*txt;
 	t_map			*map;
 	int				height;
 	Uint32			*pixels;
@@ -161,14 +138,8 @@ void				ft_fdf(t_map *map, char *map_name);
 */
 t_fdf				*ft_ptr_init(char *name);
 t_map				*ft_map_init(t_line *list);
-//t_txt				*ft_txt_init(t_fdf *img);
 int					ft_mouse_init(t_mouse *mous);
 int					ft_key_init(t_key *key);
-//t_item				*ft_sub_init(TTF_Font *font, SDL_Color color,
-		//SDL_Renderer *renderer);
-//t_item				*ft_menu_init(TTF_Font *font, SDL_Color color,
-		//SDL_Renderer *renderer);
-//t_txt				*ft_txt_init(t_fdf *img);
 void				nullify_textures(t_fdf *img);
 short				loadmedia(t_fdf *img);
 
@@ -179,14 +150,13 @@ void				ft_free_map(t_map *map);
 void				ft_free_line(t_line *list);
 void				ft_free_fdf(t_fdf *fdf);
 void				ft_clean_exit(t_fdf *img, char *err);
-//static void			ft_free_textures(t_fdf *img);
 
 /*
 **			EVENT
 */
 void				loop_til_release();
 void				ft_menu_event(t_mouse *mous, SDL_Event e);
-void				ft_keys_event(t_fdf *img, SDL_Event e/*, t_key *key*/);
+void				ft_keys_event(t_fdf *img, SDL_Event e);
 void				ft_mouse_event(t_fdf *img, t_mouse *mous, SDL_Event e);
 void				click_to_modify_map(t_map *map, t_mouse *mous);
 void				ft_save_map(t_fdf *img);
@@ -200,11 +170,15 @@ void				ft_render_button_squares(t_fdf *img);
 void				draw_menu_squares(t_fdf *img);
 void				draw_menu_textures(t_fdf *img);
 void				ft_print_pressed_button(t_fdf *img, int i);
-void				ft_print_blocks(t_pt pt, int map_value, t_fdf *img, int size);
+void				ft_print_blocks(t_pt pt, int map_value, t_fdf *img, \
+					int size);
 void				ft_parse_and_print_textures(t_fdf *img);
-void				draw_line(t_pt pt_one, t_pt pt_two, Uint32 *pixels, int colour);
-void				check_and_draw_line(t_pt start, t_pt end, t_fdf *img, int col);
-void				ft_calculate_grid_coord_and_draw_line(t_pt a, t_pt b, t_fdf *img, int col);
+void				draw_line(t_pt pt_one, t_pt pt_two, Uint32 *pixels, \
+					int colour);
+void				check_and_draw_line(t_pt start, t_pt end, t_fdf *img, \
+					int col);
+void				ft_calculate_grid_coord_and_draw_line(t_pt a, t_pt b, \
+					t_fdf *img, int col);
 void				print_button_add_and_substract(t_fdf *img);
 
 /*
@@ -226,6 +200,8 @@ void				ft_origin(t_map *map);
 float				ft_height(t_map *map);
 void				ft_fix_coords(t_map *map, int *x, int *y);
 t_pt				coord_to_pt(int x, int y);
-void				mk_point_and_trace_line(int x1, int y1, int x2, int y2, t_fdf *img);
+void				mk_point_and_trace_line(int x1, int y1, int x2, \
+					int y2, t_fdf *img);
 t_fdf				*ft_img(void);
+
 #endif
