7aa0723d57f3fe3e1192a59163dac27e19f012de
acostaz <acostaz@student.42.fr>
2020-04-22 13:59:28 +0200

    replaced gcc by clang and added -O2 on editor

--- map_editor/init/center_map.c
+++ map_editor/init/center_map.c
@@ -22,13 +22,14 @@ float		ft_height(t_map *map)
 {
 	float	height;
 
-	if (map->zmax >= 1)
+	if (map->zmax >= 1 && map->zmax < 50)
 		height = 50 / map->base_gap;
-	if (map->zmax >= 50)
+	else if (map->zmax >= 50 && map->zmax < 100)
 		height = 10 / map->base_gap;
-	if (map->zmax >= 100)
+	else if (map->zmax >= 100 && map->zmax < 500)
 		height = 1 / (2 * map->base_gap);
-	if (map->zmax >= 500)
+	//if (map->zmax >= 500)
+	else
 		height = 1 / (10 * map->base_gap);
 	return (height);
 }
